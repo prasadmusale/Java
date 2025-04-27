58be0222a28e4afb092b7d2f30754d12057e8e9f
Prasad <prasadmusale27@gmail.com>
2025-04-27 07:24:09 +0530

    "commit for updating concept1"

--- Core_Java/concept1.java
+++ Core_Java/concept1.java
@@ -28,5 +28,49 @@ public class concept1 {
             }
             System.out.println();
         }
+
+        // defining jagged array
+        int arr1[][] = new int[3][];
+
+        // defining length of each array or row which is variable of jagged array.
+        arr1[0] = new int[3];
+        arr1[1] = new int[4];
+        arr1[2] = new int[2];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        for (int n[] : arr1) {
+            for (int x : n) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+
+        // defifnig three dimensional array
+        int arr2[][][] = new int[3][4][5];
+
+        // filling random values in three dimensional array
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 5; k++) {
+                    arr2[i][j][k] = (int) (Math.random() * 100);
+                }
+            }
+        }
+
+        for (int n[][] : arr2) {
+            for (int x[] : n) {
+                for (int y : x) {
+                    System.out.print(y + ",");
+                }
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+
     }
 }
