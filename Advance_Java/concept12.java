d4b419b1daecd80e9ffb7453cd53c99f6d805d4a
Prasad <prasadmusale27@gmail.com>
2025-04-30 20:18:24 +0530

    Implemented Parallel stream with comments

--- Advance_Java/concept12.java
+++ Advance_Java/concept12.java
@@ -2,6 +2,8 @@ package Advance_Java;
 
 import java.util.List;
 import java.util.ArrayList;
+
+//This class is used to pick random values 
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -18,6 +20,7 @@ public class concept12 {
 
         // System.out.println(nums);
 
+        // This method is used to note current time in milliseconds
         long reg_stream = System.currentTimeMillis();
         int sum = nums.stream()
                 .map(n -> {
@@ -32,6 +35,8 @@ public class concept12 {
         long reg_stream__after = System.currentTimeMillis();
 
         long par_stream = System.currentTimeMillis();
+
+        // parallelStream is used to implement multiple threads to reduce time .
         int sum1 = nums.parallelStream()
                 .map(n -> {
                     try {
@@ -41,13 +46,15 @@ public class concept12 {
                     }
                     return n * 2;
                 })
+                // Stream can be of any object type so mapToInt will convert it to int type so
+                // that we can use sum() method
                 .mapToInt(i -> i)
                 .sum();
         long par_stream_after = System.currentTimeMillis();
 
+        // so it is noted that time taken by parallel stream is less than regular stream
         System.out.println(sum + " , " + sum1);
         System.out.println(reg_stream__after - reg_stream);
         System.out.println(par_stream_after - par_stream);
-
     }
 }
